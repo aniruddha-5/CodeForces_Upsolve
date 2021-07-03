30a91e5c0bfe7ec0ef22726eb169386913f5d056
aniruddha-5 <aniruddharouth@gmail.com>
2021-07-04 00:01:10 +0530

    4th July

---
+++ CF_729_A.cpp
@@ -0,0 +1,50 @@
+/* Aniruddha Routh JGEC'23 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define frr(i,a,n) for(ll i=a;i<n;i++)
+#define fr(i,a,b) for(ll i=a;i>=b;i--)
+
+#define MOD 1000000007
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define endl "\n"
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define F first
+#define S second
+#define vi vector<ll>
+#define pii pair<ll,ll>
+
+void solve()
+{
+	int n,c=0;
+	cin>>n;
+	for(int i=0;i<2*n;i++)
+	{
+		int x;
+		cin>>x;
+		if(x%2) c++;
+	}
+	if(c==n) yes
+	else no
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
+
+
+
